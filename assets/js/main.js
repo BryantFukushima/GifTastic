c23d689252a5c3a60920fb12851a62abe94a5cbe
Bryant Fukushima <bmfukushima94@gmail.com>
2018-07-06 23:11:25 -0700

    start/stop on click. Min requirement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
 var topics = ['UFC', 'baseball', 'basketball', 'football', 'soccer', 'tennis', 'wrestling', 'jiu-jitsu', 'hockey', 'golf'];
 
-for (var i in topics) {
-    var topicBtn = $('<button>');
-    topicBtn.text(topics[i]);
-    topicBtn.addClass('topic btn btn-info m-2');
-    $('#topicBtns').prepend(topicBtn);
+function topicButtons() {
+	for (var i in topics) {
+	    var topicBtn = $('<button>');
+	    topicBtn.text(topics[i]);
+	    topicBtn.addClass('topic btn btn-info m-2');
+	    $('#topicBtns').append(topicBtn);
+	}
 }
+topicButtons();
 
 var offset = 0;
 
@@ -22,12 +25,18 @@ function apiSearch() {
 	        	var gifDiv = $('<div>');
 	        	gifDiv.addClass('col-4 showGif');
 	        	var gif = $('<img>');
-		        gif.attr('src', response.data[j].images.original.url);
+	        	gif.addClass('gifImg');
+		        gif.attr('src', response.data[j].images.original_still.url);
+		        gif.attr('data-still' , response.data[j].images.original_still.url);
+		        gif.attr('data-animate' , response.data[j].images.original.url);
+		        gif.attr('data-start' , false);
 		        var rating = $('<p>');
 		        rating.text('Rating: ' + response.data[j].rating);
 		        gifDiv.append(gif);
 		        gifDiv.prepend(rating)
 		        $('#display').append(gifDiv);
+
+		       
 		    }
         
     });
@@ -57,16 +66,31 @@ $('#addTopic').on('click' , function() {
 	var newTopic = $('#addVal').val()
 
 	if (newTopic != "") {
-		var topicBtn = $('<button>');
-	    topicBtn.text(newTopic);
-	    topicBtn.addClass('topic btn btn-info m-2');
-	    $('#topicBtns').append(topicBtn);
-	    offset = 0;
+		$('#topicBtns').empty();
+		topics.push(newTopic);
+		topicButtons();
+		offset = 0;
 		search = newTopic;
 		$('#display').empty();
 		apiSearch();
 	}
 
 	$('#addVal').val("");
+
+});
+
+$(document).on('click' , '.gifImg' , function() {
+
+	var animate = $(this).attr('data-animate');
+	var still = $(this).attr('data-still');
+
+	if ($(this).attr('data-start') == "false") {
+
+		$(this).attr('data-start' , true);
+		$(this).attr('src' , animate);
+	} else {
+		$(this).attr('data-start' , false);
+		$(this).attr('src' , still);
+	}
 
 });
